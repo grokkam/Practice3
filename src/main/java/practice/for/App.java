d04b8c1b63997966b226f1fa3cded3e9c8cf102b
Govardhan <grokkam@everesttech.com>
2021-11-12 14:12:27 +0530

    first commit

---
+++ src/main/java/practice/for/App.java
@@ -0,0 +1,13 @@
+package practice.for;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
